bb6102c1b49d173077c32120741977362673a9e2
lightwavebusiness <leon@l-w-b.co>
2020-06-19 09:30:30 +0100

    id

--- mesh.js
+++ mesh.js
@@ -77,7 +77,7 @@
     ext.messageReceived = function(callback) {
         fb.child('messages').on('child_added', function(snapshot, preChildKey) {
             let message = snapshot.val()
-            window["new-message-"][message.id] = message
+            window["new-message-"+message.id] = message
             window["new-message-id"] = message.id
         });
 
